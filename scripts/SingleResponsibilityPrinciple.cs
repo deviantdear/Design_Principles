9ff890979ecd80d845cee47ca3a2c41cf8501356
Valarie Adams <deviantdear@gmail.com>
2018-12-09 10:50:51 -0700

    Update SingleResponsibilityPrinciple.cs
    Journal scripts to illustrate single responsibility principle

--- scripts/SingleResponsibilityPrinciple.cs
+++ scripts/SingleResponsibilityPrinciple.cs
@@ -9,9 +9,50 @@ using static System.Console;
 
 namespace DesignPrinciples
 {
+    #region Description of Principle
+    //Every class should have a single responsibility over a part of the functionality provided by the software it is designed for
+    #endregion
 
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}:{text}");
+            return count; //memento pattern
+        }
+
+        //not a stable way to remove entries because it removes the other indices
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Joing(Environment.NewLine, entries);
+        }
+
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        //public static 
+    }
     public class SingleResponsibilityPrinciple
     {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("It was sunny, and beautiful");
+            j.AddEntry("Woe is me");
+
+            WriteLine(j);
+        }
         public SingleResponsibilityPrinciple()
         {
         }

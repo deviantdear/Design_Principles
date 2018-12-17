48869ce79397a142cc15a3403186ab757e7cee02
Valarie Adams <deviantdear@gmail.com>
2018-12-17 16:06:14 -0700

    Added HTML Builder example
    Created a simple HTML Builder

--- scripts/Builder/Builder/Demo.cs
+++ scripts/Builder/Builder/Demo.cs
@@ -9,12 +9,92 @@ using static System.Console;
 
 namespace DesignPrinciples
 {
+    //Creating an HTML builder class
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int indentSize = 2;
+
+        public HtmlElement()
+        {
+           
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
+            Name = name;
+            Text = text;
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new StringBuilder(' ', indentSize * indent);
+            sb.AppendLine($"{i}<{Name}>");
+
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var e in Elements)
+            {
+                sb.Append(e.ToStringImpl(indent + 1));
+            }
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+
+    }
+
+    public class HtmlBuilder
+    {
+        HtmlElement root = new HtmlElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            rootName.Name = rootName;
+        }
+
+        public HtmlBuilder AddChild(string childName, string childText)
+        {
+            //Shows overall design of builder but doesn't scale the greatest
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+
+            return this;
+        }
+
+        public override string ToSring()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement {Name = rootName};
+        }
+    }
+
     public class Demo
     {
 
         static void Main(String[] args)
         {
-
+            var builder = new HtmlBuilder("ul");
+            //fluent interface that chains several calls by returning a reference to the builder
+            builder.AddChild("li", "hello").AddChild("li", "world");
+            WriteLine(builder.ToString());
         }
     }
 }

066221f6ec9aebe229d2932ab9b52de8f7a396f0
Valarie Adams <deviantdear@gmail.com>
2019-06-05 10:10:15 -0700

    started strategy pattern exercise

---
+++ scripts/StrategyPattern.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace DesignPatterns { 
+
+    public enum OutputFormat
+    {
+        Markdown,
+        Html
+    }
+
+    // <ul><li>foo<//li><ul>
+    public interface IListStrategy
+    {
+        void Start(StringBuilder sb);
+        void End(StringBuilder sb);
+        void AddListItem(StringBuilder sb, string item);
+
+    }
+
+    public class HTMLListStrategy: IListStrategy
+    {
+        public void Start(StringBuilder sb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class StrategyPattern
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+	    public StrategyPattern()
+	    {
+
+	    }
+
+
+    }// end class
+
+} //end namespace
